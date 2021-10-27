0d369dce0227323f45bc5f15b0e0f0aaa697ba63
Cate <aeyuk@crimson.ua.edu>
2021-10-27 16:47:57 -0500

    Create navbar with bootstrap

--- src/components/style/Header.jsx
+++ src/components/style/Header.jsx
@@ -1,8 +1,22 @@
+import { Link } from 'react-router-dom';
+
 function Header() {
     return (
-        <div>
-            Header <hr/>
-        </div>
+        <header>
+            <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                <div>
+                    <a href="https://www.in28minutes.com" className="navbar-brand">in28Minutes</a>
+                </div>
+                <ul className="navbar-nav">
+                    <li className="nav-link"><Link to="/welcome/cate">Home</Link></li>
+                    <li className="nav-link"><Link to="/todos">Todo List</Link></li>
+                </ul>
+                <ul className="navbar-nav navbar-collapse justify-content-end">
+                    <li className="nav-link"><Link to="/login">Login</Link></li>
+                    <li className="nav-link"><Link to="/logout">Logout</Link></li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
